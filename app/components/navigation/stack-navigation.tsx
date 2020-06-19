13de518cbdd71a82d46c24eb6b11f8a3f14963d2
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-06-19 18:40:15 +0100

    Dynamic title

--- app/components/navigation/stack-navigation.tsx
+++ app/components/navigation/stack-navigation.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import {Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Products from '../products';
+import {COLOURS} from '../../utils/colours';
+import Fonts from '../../utils/fonts';
 import Details from '../details';
 import {RootStackParamList} from './types';
+
+const HEADER_STYLE = {
+  headerStyle: {
+    backgroundColor: COLOURS.ternary,
+  },
+  headerTintColor: COLOURS.white,
+  headerTitleStyle: {
+    ...Fonts.grand,
+    fontSize: 32,
+  },
+};
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const StackNavigation = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Products" component={Products} />
-    <Stack.Screen name="Details" component={Details} />
+    <Stack.Screen
+      options={() => ({
+        ...HEADER_STYLE,
+      })}
+      name="Products"
+      component={Products}
+    />
+    <Stack.Screen
+      name="Details"
+      options={({route}) => ({
+        headerTitle: route.params.item.title,
+        headerBackTitleVisible: false,
+        headerRight: () => <Text>'HI'</Text>,
+        ...HEADER_STYLE,
+      })}
+      component={Details}
+    />
   </Stack.Navigator>
 );
 
