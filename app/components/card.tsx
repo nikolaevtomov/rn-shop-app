b4884d8becbee1d76d23be672383b3eb7d18860f
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-06-20 20:29:47 +0100

    Sum of purchese baske

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 import {connect} from 'react-redux';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 
@@ -13,26 +13,35 @@ interface OwnProps {
 
 interface StateProps {
   card: Array<ProductProps>;
+  total: number;
 }
 
 interface DispatchProps {}
 
 type Props = OwnProps & StateProps & DispatchProps;
 
-const Card: React.FunctionComponent<Props> = ({}) => {
+const Card: React.FunctionComponent<Props> = ({total}) => {
   return (
     <View style={styles.screen}>
-      <Text style={Fonts.jack}>Card</Text>
+      <Text style={Fonts.jack}>Total: £{total}</Text>
+      <View style={styles.buttonsWrapper}>
+        <Button title="Order now" onPress={() => {}} />
+        <Button title="Clear card" onPress={() => {}} />
+      </View>
     </View>
   );
 };
 
 const mapStateToProps = (state: StoreState): StateProps => ({
   card: state.card,
+  total: state.card.reduce((acc: number, curr: ProductProps) => {
+    return acc + curr.price;
+  }, 0),
 });
 
 export default connect(mapStateToProps, {})(Card);
 
 const styles = StyleSheet.create({
   screen: {},
+  buttonsWrapper: {},
 });
